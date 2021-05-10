6fb8e6e8d2dee5bc66e3e05945f0968c8aafb974
Michael O'Ryan <michael.oryan@gmail.com>
2021-05-10 16:18:20 +1000

    Import fizzbuzz class

--- test/fizzbuzz.spec.ts
+++ test/fizzbuzz.spec.ts
@@ -1,3 +1,9 @@
 import { expect } from 'chai';
 import mocha from 'mocha';
 
+import Fizzbuzz from './../src/fizzbuzz';
+
+
+describe('Can import fizzbuzz', function () {
+  const Fizzbuzz = new Fizzbuzz();
+});
