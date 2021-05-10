f144038d028cfa27d8359817a75fb21cbc216e24
Michael O'Ryan <michael.oryan@gmail.com>
2021-05-10 17:38:14 +1000

    Pass 5 in a row end with one Buzz

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,12 +1,14 @@
 'use strict';
 
 export default class Fizzbuzz {
+
   whatToSay(turn:number):string {
-        
+
     if(turn > 3 && turn % 3 === 0)
       return 'Fizz';
 
     switch(turn) {
+    case 10: return 'Buzz'; break;
     case 5: return 'Buzz'; break;
     case 3: return 'Fizz'; break;
     case 2: return '2'; break;
