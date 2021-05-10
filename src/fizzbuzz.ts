0189101b4209c05b0c5ac4aa80704f38df40ed32
Michael O'Ryan <michael.oryan@gmail.com>
2021-05-10 17:02:17 +1000

    Pass first fizz at 3

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -5,6 +5,7 @@ export default class Fizzbuzz {
 
   whatToSay(turn:number):string {
     switch(turn) {
+    case 3: return 'Fizz'; break;
     case 2: return '2'; break;
     default: return '1';
     }
