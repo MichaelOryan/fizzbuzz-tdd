d695396ec611f9a06587a091e4bd7520e2050d45
Michael O'Ryan <michael.oryan@gmail.com>
2021-05-10 17:31:24 +1000

    Pass start @ 1 for congruent test Fizz

--- src/fizzbuzz.ts
+++ src/fizzbuzz.ts
@@ -1,21 +1,17 @@
 'use strict';
 
 export default class Fizzbuzz {
-
-    counter = 0;
-
-    whatToSay(turn:number):string {
-      this.counter++;
+  whatToSay(turn:number):string {
         
-      if(turn > 3 && turn % 3 === 1)
-        return 'Fizz';
+    if(turn > 3 && turn % 3 === 0)
+      return 'Fizz';
 
-      switch(turn) {
-      case 5: return 'Buzz'; break;
-      case 3: return 'Fizz'; break;
-      case 2: return '2'; break;
-      default: return '1';
-      }
+    switch(turn) {
+    case 5: return 'Buzz'; break;
+    case 3: return 'Fizz'; break;
+    case 2: return '2'; break;
+    default: return '1';
     }
+  }
 
 }
