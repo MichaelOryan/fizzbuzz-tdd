a8725faa263066beb5be7dc6cd930edc3ea7ea55
Michael O'Ryan <michael.oryan@gmail.com>
2021-05-10 17:33:14 +1000

    Refactor test order for Fizz and Buzz

--- test/fizzbuzz.spec.ts
+++ test/fizzbuzz.spec.ts
@@ -35,58 +35,64 @@ describe('Can import fizzbuzz', function () {
         fizzbuzz.whatToSay(3)
       ).to.be.equal('Fizz');
     });
+    describe('Fizz', function () {
 
-    it('Three numbers in a row should only produce only one result starting with Fizz', function(){
-      const startingNumber = 4;
-      let fizzCount = 0;
-      for(let count = 0; count < 3; count++) 
-        fizzCount += fizzbuzz.whatToSay(startingNumber + count).startsWith('Fizz') ? 1 : 0;
-
-      expect(
-        fizzCount
-      ).to.be.equal(1);
-    });
-
-    it('5 becomes Buzz', function(){
-      expect(
-        fizzbuzz.whatToSay(5)
-      ).to.be.equal('Buzz');
-    });
-
-    it('Three numbers in a row should only produce only one result starting with Fizz. Random starting number', function(){
-      const startingNumber = Math.floor(Math.random() * 100) + 1;
-      let fizzCount = 0;
-      for(let count = 0; count < 3; count++) 
-        fizzCount += fizzbuzz.whatToSay(startingNumber + count).startsWith('Fizz') ? 1 : 0;
-
-      expect(
-        fizzCount
-      ).to.be.equal(1);
-    });
-
-
-    it('Any three numbers n, n+3, n+6 should all or none start with Fizz.', function(){
-      const randomStartingNumber = Math.floor(Math.random() * 100) + 1;
-      for(let startingNumber = randomStartingNumber; startingNumber < randomStartingNumber + 20; startingNumber++) {
+      it('Three numbers in a row should only produce only one result starting with Fizz', function(){
+        const startingNumber = 4;
         let fizzCount = 0;
         for(let count = 0; count < 3; count++) 
-          fizzCount += fizzbuzz.whatToSay(startingNumber + (count * 3)).startsWith('Fizz') ? 1 : 0;
+          fizzCount += fizzbuzz.whatToSay(startingNumber + count).startsWith('Fizz') ? 1 : 0;
+
         expect(
           fizzCount
-        ).to.be.oneOf([0, 3]);  
-      }
-    });
+        ).to.be.equal(1);
+      });
 
-    it('Any three numbers n, n+3, n+6 should all or none start with Fizz. Start @ 1', function(){
-      const randomStartingNumber = 1;
-      for(let startingNumber = randomStartingNumber; startingNumber < randomStartingNumber + 20; startingNumber++) {
+
+      it('Three numbers in a row should only produce only one result starting with Fizz. Random starting number', function(){
+        const startingNumber = Math.floor(Math.random() * 100) + 1;
         let fizzCount = 0;
         for(let count = 0; count < 3; count++) 
-          fizzCount += fizzbuzz.whatToSay(startingNumber + (count * 3)).startsWith('Fizz') ? 1 : 0;
+          fizzCount += fizzbuzz.whatToSay(startingNumber + count).startsWith('Fizz') ? 1 : 0;
+
         expect(
           fizzCount
-        ).to.be.oneOf([0, 3]);  
-      }
+        ).to.be.equal(1);
+      });
+
+
+      it('Any three numbers n, n+3, n+6 should all or none start with Fizz.', function(){
+        const randomStartingNumber = Math.floor(Math.random() * 100) + 1;
+        for(let startingNumber = randomStartingNumber; startingNumber < randomStartingNumber + 20; startingNumber++) {
+          let fizzCount = 0;
+          for(let count = 0; count < 3; count++) 
+            fizzCount += fizzbuzz.whatToSay(startingNumber + (count * 3)).startsWith('Fizz') ? 1 : 0;
+          expect(
+            fizzCount
+          ).to.be.oneOf([0, 3]);  
+        }
+      });
+
+      it('Any three numbers n, n+3, n+6 should all or none start with Fizz. Start @ 1', function(){
+        const randomStartingNumber = 1;
+        for(let startingNumber = randomStartingNumber; startingNumber < randomStartingNumber + 20; startingNumber++) {
+          let fizzCount = 0;
+          for(let count = 0; count < 3; count++) 
+            fizzCount += fizzbuzz.whatToSay(startingNumber + (count * 3)).startsWith('Fizz') ? 1 : 0;
+          expect(
+            fizzCount
+          ).to.be.oneOf([0, 3]);  
+        }
+      });
+    });
+    
+    describe('Buzz', function () {
+      it('5 becomes Buzz', function(){
+        expect(
+          fizzbuzz.whatToSay(5)
+        ).to.be.equal('Buzz');
+      });
+
     });
 
   });
