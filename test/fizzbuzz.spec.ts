afe05ca60bc44a16ee956ea59f314ecd9259bbe0
Michael O'Ryan <michael.oryan@gmail.com>
2021-05-10 16:50:51 +1000

    Has function to determine what to say

--- test/fizzbuzz.spec.ts
+++ test/fizzbuzz.spec.ts
@@ -5,10 +5,18 @@ import Fizzbuzz from './../src/fizzbuzz';
 
 
 describe('Can import fizzbuzz', function () {
-  let fizzbuzz:Fizzbuzz;
+  const fizzbuzz:Fizzbuzz = new Fizzbuzz();
   it('Can create fizzbuzz class instance', function(){
     expect(
       fizzbuzz instanceof Fizzbuzz
     ).to.be.equal(true);
+  });
+  describe('"whatToSay" function to convert number to thing to say', function () {
+
+    it('Has "whatToSay" function', function(){
+      expect(
+        fizzbuzz.whatToSay
+      ).to.be.a('function');
+    });
   });
 });
